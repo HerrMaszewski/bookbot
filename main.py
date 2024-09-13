b7bb49275f1d5201b28a4abe94f664718be2c4b1
HerrMaszewsk <tadmaszewski@gmail.com>
2024-09-13 20:50:56 +0200

    word count

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+import os
+print("Current working directory:", os.getcwd())
+
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    count = get_word_count(text)
+    
+    print(f"Number of words: {count}")
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
+def get_word_count(text):
+    words = text.split()
+    count = len(words)
+    return count
 
 
 main()
